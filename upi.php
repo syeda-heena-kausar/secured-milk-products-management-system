a0d3851eeb3003080114222b1f910ac7a18fb88b
syeda-heena-kausar <88453095+syeda-heena-kausar@users.noreply.github.com>
2021-09-02 13:06:09 +0530

    Add files via upload

---
+++ upi.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+error_reporting(0);
+if(strlen($_SESSION['alogin'])==0)
+  { 
+header('location:index.php');
+}
+else{
+$oid=intval($_GET['oid']);
+if(isset($_POST['submit2'])){
+$status=$_POST['status'];
+$remark=$_POST['remark'];//space char
+
+<img src="C:\Users\BI BI AYESHA\Downloads\Capture.jpg"/>
+//echo "<script>alert('Order updated sucessfully...');
+//</script>";
+}
+</body>
+</html>
+<?php  }?>
+
+     
